20f8d02ab4a11783f049306d021ea471977400c1
Hoang Thien Dang Nguyen <159185239+dang-2nguyen@users.noreply.github.com>
2024-03-25 18:14:11 +0000

    dfgdrgdr

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Dang All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Dang
+// Created on: Mar 2024
 // This file contains the JS functions for index.html
+"use strict"
+
+/**
+ * This function calculates area of a triangle.
+ */const TAX_RATE = 0.18
+function calculateTheSalary () {
+  
+  // input
+  const time = parseInt(document.getElementById('the-time').value)
+  const money = parseInt(document.getElementById('salary').value)
+
+  // process
+  const pay = (time*money)*(1.00- TAX_RATE )
+  const government = (time * money) * TAX_RATE
+  
+  // output
+  document.getElementById('pay').innerHTML = 'Your pay will be : ' +'$' + pay.toFixed(2)  
+  document.getElementById('goverment').innerHTML = 'the government will take : '+ '$' + government.toFixed(2) 
+}
